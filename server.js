88a3dbbdf7118ab2d9dd4696c0e64dbccafa14ad
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-15 16:52:00 -0400

    Working on writing notes

--- server.js
+++ server.js
@@ -15,3 +15,12 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 
+app.post('/api/note', (req, res) => {
+    return this.util.promisify(fs.writeFile("db/db.json", JSON.stringify(note)))
+});
+app.get('/api/note', (req, res) => {
+    return this.util.promisify(fs.readFile("db/db.json", "utf8"))
+});
+app.delete('/api/note', (req, res) => {
+
+});
