11ed099c0e49a0db535159af23f537bb5e253e56
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-15 16:35:56 -0400

    html paths

--- server.js
+++ server.js
@@ -15,20 +15,3 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 
-
-
-app.get("/api/notes", (req, res) =>
-    notes.getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(error))
-);
-app.post("/api/notes",  (req, res) =>
-    notes.addNotes(req.body)
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(error))
-);
-app.delete("/api/notes/:id",  (req, res) =>
-    notes.removeNote(req.params.id)
-    .then(() => res.json({ok: true}))
-    .catch(err => res.status(500).json(error))
-);
